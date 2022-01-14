b4f1e996ec71e83cc882e4e637f7b6f5ba6de463
LeifAG <leif.hammar@gymnasium.ax>
2022-01-14 12:51:44 +0200

    mera kommentarer

--- js/fetchTask.js
+++ js/fetchTask.js
@@ -1,3 +1,4 @@
+//hämtar lista med tasks sidvis
 function getTasklistPages(page) {
     fetch('https://www.datanom.ax/~kjell/Tidsredovisning/getTasklist.php?page=' + page)
         .then(function (response) {
@@ -10,12 +11,33 @@ function getTasklistPages(page) {
         })
 }
 
+//hämtar lista med tasks baserat på datum
+function getTasklistDates() {
+    let startDate = document.getElementById("startDate").value;
+    let stopDate = document.getElementById("stopDate").value;
+    
+    fetch('https://www.datanom.ax/~kjell/Tidsredovisning/getTasklist.php?from=' + startDate+"&to="+stopDate)
+    .then(function (response) {
+        if (response.status == 200) {
+            return response.json();
+        }
+    })
+    .then(function (data) {
+        appendRows(data);
+    })
+}
+
+//lägger till rader av tasks till tabell på sidan
 function appendRows(data, page = 0) {
     let tablebody, pagenav, tr, td_task, td_datum, td_tid, td_radera, td_desc, pages, tasks;
     
+    //skapar tasks array från hämtningen data
     tasks = data.tasks;
+
+    //laggrar antalet sidor om hämtningen är med sidor
     if (page != 0) { pages = data.pages; }
 
+    //tömmer sidnav div
     pagenav = document.getElementById("pagenav");
     pagenav.innerHTML = "";
 
@@ -23,6 +45,7 @@ function appendRows(data, page = 0) {
     tablebody = document.getElementById("tasktablebody")
     tablebody.innerHTML = "";
 
+    //loppar genom tasks arrayn och skapar rader
     for (let i = 0; i < tasks.length; i++) {
 
         //skapa tabellrad med id row# för elementet
@@ -58,16 +81,19 @@ function appendRows(data, page = 0) {
         tablebody.appendChild(tr);
     }
 
+    //kallar på sidnav function om hämtningen är av typen sidor
     if (page != 0) { appendPageNav(pagenav, page, pages); }
 
 }
 
+//skapar sidnavigering bland tasks
 function appendPageNav(pagenav, page, pages) {
     let ul, li_ff, li_f, li_n, li_nn;
     console.log("page=" + page + " and pages=" + pages);
 
     ul = document.createElement('ul');
 
+    //lägger till olika fram och bakåt knappar beroende på vilken sida man är på
     if (page != 1 && page != 2) {
         li_ff = document.createElement('li');
         li_ff.innerHTML = "&lt;&lt;";
@@ -96,13 +122,7 @@ function appendPageNav(pagenav, page, pages) {
     pagenav.appendChild(ul);
 }
 
-window.onload = function () {
-    setDates();
-    getTasklistPages(1);
-    document.getElementById("dateGet").addEventListener("click",function(){getTasklistDates()});
-    document.getElementById("pageGet").addEventListener("click",function(){getTasklistPages(1)});
-}
-
+//sätter default värden på datumfälten på sidan, dagens datum och ett datum bakåt i tiden
 function setDates() {
     let date = new Date();
     document.getElementById("stopDate").value = date.toISOString().split('T')[0];
@@ -110,38 +130,10 @@ function setDates() {
     document.getElementById("startDate").value = date.toISOString().split('T')[0];
 }
 
-function getTasklistDates() {
-    let startDate = document.getElementById("startDate").value;
-    let stopDate = document.getElementById("stopDate").value;
-    
-    fetch('https://www.datanom.ax/~kjell/Tidsredovisning/getTasklist.php?from=' + startDate+"&to="+stopDate)
-    .then(function (response) {
-        if (response.status == 200) {
-            return response.json();
-        }
-    })
-    .then(function (data) {
-        appendRows(data);
-    })
+//när sidan laddats startas js funtioner
+window.onload = function () {
+    setDates();
+    getTasklistPages(1);
+    document.getElementById("dateGet").addEventListener("click",function(){getTasklistDates()});
+    document.getElementById("pageGet").addEventListener("click",function(){getTasklistPages(1)});
 }
-
-    /*
-    let startDate = document.getElementById("startdate").value;
-    let stopDate = document.getElementById("stopdate").value;
-
-    fetch('https://www.datanom.ax/~kjell/getTasklist.php?from=' + startDate + '&to=' + stopDate)
-        .then(function (response) {
-            if (response.status == 200) {
-                return response.json();
-            } else {
-                throw { status: response.status, message: response.json() };
-            }
-        })
-        .then(function (data) {
-            appendRows(data);
-        })
-        .catch(function (error) {
-            alert(error);
-        });
-}
-*/
